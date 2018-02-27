71f7ce5265431638405baae54f8723246be7d7a6
axl411 <axl411511@live.com>
2018-02-27 23:19:39 +0900

    update

--- ProfileCard/App.js
+++ ProfileCard/App.js
@@ -4,55 +4,55 @@
  * @flow
  */
 
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 import {
   Platform,
   StyleSheet,
   Text,
-  View
-} from 'react-native';
+  View,
+} from 'react-native'
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
-
-type Props = {};
-export default class App extends Component<Props> {
-  render() {
+export default class App extends Component<{}> {
+  render () {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <View style={styles.cardContainer}>
+          <View style={styles.cardImageContainer}>
+            {/*<Image style={styles.cardImage} source={require('./user.png')}/>*/}
+          </View>
+        </View>
       </View>
-    );
+    )
   }
 }
+
+const profileCardColor = 'dodgerblue'
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
+    alignItems: 'center'
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  cardContainer: {
+    borderColor: 'black',    // C
+    borderWidth: 3,
+    borderStyle: 'solid',
+    borderRadius: 20,
+    backgroundColor: profileCardColor,
+    width: 300,
+    height: 400
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  cardImageContainer: {   // D
+    backgroundColor: 'white',
+    borderWidth: 3,
+    borderColor: 'black',
+    width: 120,
+    height: 120,
+    borderRadius: 60,
   },
+  cardImage: {    // E
+    width: 80,
+    height: 80
+  }
 });
