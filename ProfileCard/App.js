40024ae6a0a515bbb4793bb54f1bb2db65649156
axl411 <axl411511@live.com>
2018-03-24 18:30:05 +0900

    update

--- ProfileCard/App.js
+++ ProfileCard/App.js
@@ -4,11 +4,17 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { Image, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react'
+import {
+  Image,
+  StyleSheet,
+  Text,
+  View,
+  Platform
+} from 'react-native'
 
 export default class App extends Component<{}> {
-  render() {
+  render () {
     return (
       <View style={styles.container}>
         <View style={styles.cardContainer}>
@@ -27,12 +33,13 @@ export default class App extends Component<{}> {
           </View>
           <View>
             <Text style={styles.cardDescription}>
-              John is a really great JavaScript developer. He loves using JS to build React Native applications for iOS and Android.
+              John is a really great JavaScript developer. He loves using JS to
+              build React Native applications for iOS and Android.
             </Text>
           </View>
         </View>
       </View>
-    );
+    )
   }
 }
 
@@ -42,7 +49,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   cardContainer: {
     alignItems: 'center',
@@ -52,7 +59,19 @@ const styles = StyleSheet.create({
     borderRadius: 20,
     backgroundColor: profileCardColor,
     width: 300,
-    height: 400
+    height: 400,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: {
+          height: 10
+        },
+        shadowOpacity: 1
+      },
+      android: {
+        elevation: 15
+      }
+    }),
   },
   cardImageContainer: {
     alignItems: 'center',
@@ -63,11 +82,25 @@ const styles = StyleSheet.create({
     height: 120,
     borderRadius: 60,
     marginTop: 30,
-    paddingTop: 15
+    paddingTop: 15,
+    ...Platform.select({
+      ios: {
+        shadowColor: 'black',
+        shadowOffset: {
+          height: 10
+        },
+        shadowOpacity: 1
+      },
+      android: {
+        borderWidth: 3,
+        borderColor: 'black',
+        elevation: 15
+      }
+    })
   },
   cardImage: {
     width: 80,
-    height: 80
+    height: 80,
   },
   cardName: {
     color: 'white',
@@ -77,13 +110,13 @@ const styles = StyleSheet.create({
     textShadowColor: 'black',  // A
     textShadowOffset: {        // B
       height: 2,
-      width: 2
+      width: 2,
     },
     textShadowRadius: 3        // C
   },
   cardOccupationContainer: {
     borderColor: 'black',
-    borderBottomWidth: 3
+    borderBottomWidth: 3,
   },
   cardOccupation: {
     fontWeight: 'bold',
@@ -95,6 +128,6 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginRight: 40,
     marginLeft: 40,
-    marginBottom: 10
-  }
-});
+    marginBottom: 10,
+  },
+})
